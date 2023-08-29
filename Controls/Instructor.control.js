fab3dcb90c504c829ec81f1f3abc15ec860beb7e
sonuparjapat <sahil992001parjapat@gmail.com>
2023-08-29 15:45:21 +0530

    check

--- Controls/Instructor.control.js
+++ Controls/Instructor.control.js
@@ -22,7 +22,7 @@ InstructorRouter.get("/instructers",async(req,res)=>{
     }
   
     try{
-       const maindata=await instructorModel.find()
+       const maindata=await instructorModel.find(querydata)
     if(limit&&page){
         const data=await instructorModel.find(querydata).sort({[sort]:order=="asc"?"1":order=="desc"?"-1":""}).skip((page-1)*limit).limit(limit)
         res.status(200).json({msg:data,totalpages:Math.ceil(maindata.length/limit)}) 
